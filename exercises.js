9885a4bf8d0f741f5e1d4c8758e2e671937f8415
Bradley <brad@Bradleys-MacBook-Pro.local>
2017-09-09 08:53:45 -1000

    finished ex 13

--- exercises.js
+++ exercises.js
@@ -252,11 +252,17 @@ console.log("addsub ver2: ", addThenSubtract(bango3,bango4,bango5))
 */ 
 
 function multiplyThenDivide(num1, num2, num3){
-	return multiply(num1, num2) / num3;
+	var multi = multiply(num1, num2);
+	return multi / num3;
 }
 
-console.log(multiplyThenDivide(bango3, bango4, bango5));
+console.log("multidiv: ",multiplyThenDivide(bango3, bango4, bango5));
 
+function multiplyThenDivide(num1,num2, num3){
+	return divide(multiply(num1,num2), num3);
+
+}
+console.log("multidiv2: ", multiplyThenDivide(bango3, bango4, bango5))
 
 
 /*
@@ -271,6 +277,14 @@ console.log(multiplyThenDivide(bango3, bango4, bango5));
  *  This function `returns` back a string which represents someone's full name. Invoke this function by passing in your first  and last name into the function. Store the return value into a variable named `myFullName` and console.log this variable to show your result.
 */
 
+function createFullName(firstName, lastName){
+return firstName + lastName;
+
+
+}
+ 
+ var myFullName = createFullName("Bradley"," Foster");
+ console.log(myFullName)
 
 
 /*
@@ -287,6 +301,11 @@ console.log(multiplyThenDivide(bango3, bango4, bango5));
  * Console.log your result.
  */
 
+ function eatFood(firstName, lastName, food){
+return createFullName(firstName, lastName) + " eats " + food + "everyday for breakfast";
+ }
+
+console.log(eatFood("Bradley ", "Foster", "eggs "))
 
 
 /************** ENGAGE HYPERDRIVE **************/
